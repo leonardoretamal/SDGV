78c9efb2e21fb3e483c7e4410942e811d1b677b1
jama2210 <josemaldo2214@gmail.com>
2024-06-14 14:55:12 -0400

    captcha

---
+++ app/controllers/reCaptcha/keys.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $claves = [
+        //Esto es para JavaScript
+        'publica' => '6LethvMpAAAAAC3PxTYmX3-6nVdGbNgGoSS6N_8r',
+        'privada' => '6LethvMpAAAAAJ3DTT5mgKE6GKMuiI_SKi3SBXNP'
+        //Esto es para PHP
+    ];
+
+?>
